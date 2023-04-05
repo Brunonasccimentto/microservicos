42d8a415a8e154a544ac44528c358c15e64693cb
Bruno Rodrigues <104613598+Brunonasccimentto@users.noreply.github.com>
2023-04-05 18:44:28 -0300

    Delete router directory

--- router/Routers.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const formController = require("../controller/formcontrolers")
-const router = express.Router()
-
-router.get("/tipos", formController.getTypes) //consultar tipos de cliente
-
-module.exports = router
